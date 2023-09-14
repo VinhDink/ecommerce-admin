1d537ddd8c3789159beca4f8203292c564cff6ef
vinh <vinh@vinhs-Laptop.local>
2023-09-14 16:04:06 +0700

    finish warehouse admin

---
+++ src/pages/CategoryManager.js
@@ -0,0 +1,207 @@
+import React, { useState, useEffect } from "react";
+import { Container, Table, Button, Modal, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "../config";
+
+const CategoryManager = () => {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [showModal, setShowModal] = useState(false);
+  const [showCreate, setShowCreate] = useState(false);
+  const [name, setName] = useState("");
+  const [_id, setId] = useState("");
+  const [image, setImage] = useState("");
+  const [createName, setCreateName] = useState("");
+  const [att, createAtt] = useState("");
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get("/category");
+        setCategories(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+        setLoading(false);
+      }
+    };
+
+    getData();
+  }, []);
+
+  const handleAddCategory = (id) => {
+    console.log("Add Category clicked");
+  };
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setImage(file);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    console.log("hello");
+    await axios.put(`/category`, {
+      name: name,
+      _id: _id,
+      attCate: att,
+    });
+    closeUpdateModal();
+  };
+
+  const handleCreate = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append("name", createName);
+    formData.append("image", image);
+
+    try {
+      const response = await axios.post("category", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data", // Set the content type for file upload
+        },
+      });
+
+      if (response.status === 200) {
+        // Handle success
+        console.log("Category created successfully!");
+      } else {
+        // Handle error
+        console.error("Failed to create product.");
+      }
+    } catch (error) {
+      console.error("An error occurred:", error);
+    }
+
+    closeCreateModal();
+  };
+
+  const openUpdateModal = (name, id) => {
+    setId(id);
+    setName(name);
+    setShowModal(true);
+    console.log(name.name);
+  };
+
+  const closeUpdateModal = () => {
+    setName(name);
+    setShowModal(false);
+  };
+
+  const openCreateModal = (name, id) => {
+    setShowCreate(true);
+  };
+
+  const closeCreateModal = () => {
+    setShowCreate(false);
+  };
+
+  const deleteCate = async(id) => {
+    await axios.post(`/delete-category`, {
+        _id: id,
+    });
+    window.location.reload();
+  }
+
+  return (
+    <Container>
+      <div class="d-flex justify-content-between mb-5 mt-3">
+        <h1>Category Management</h1>
+        <Button variant="primary" onClick={openCreateModal}>
+          Add Category
+        </Button>
+      </div>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {categories.map((category) => (
+            <tr key={category.id}>
+              <td>{category.name}</td>
+              <td>
+                <Button
+                  variant="info"
+                  onClick={() => openUpdateModal(category, category._id)}
+                >
+                  Update
+                </Button>
+                <Button
+                  variant="danger"
+                  onClick={() => deleteCate(category._id)}
+                >
+                    Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Modal show={showModal} onHide={closeUpdateModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Category</Modal.Title>
+        </Modal.Header>
+        <form className="pm__modal-form" onSubmit={handleSubmit}>
+          <Modal.Body>
+            <input value={_id} hidden />
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              placeholder="Product name"
+              className="pm__form-input"
+              value={name.name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeUpdateModal}>
+              Close
+            </Button>
+            <button class="btn btn-primary">Save Changes</button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+      <Modal show={showCreate} onHide={closeCreateModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Update Category</Modal.Title>
+        </Modal.Header>
+        <form className="pm__modal-form" onSubmit={handleCreate}>
+          <Modal.Body>
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              id="createName"
+              name="createName"
+              placeholder="Category name"
+              className="pm__form-input"
+              value={createName.name}
+              onChange={(e) => setCreateName(e.target.value)}
+            />
+            <label htmlFor="image">Product Image</label>
+            <input
+              type="file"
+              name="image"
+              accept=".jpg, .jpeg, .png"
+              onChange={handleFileChange}
+            />
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={closeCreateModal}>
+              Close
+            </Button>
+            <button class="btn btn-primary">Save Changes</button>
+          </Modal.Footer>
+        </form>
+      </Modal>
+    </Container>
+  );
+};
+
+export default CategoryManager;
